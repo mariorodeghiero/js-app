0220009c88c7c59e756d3acaeb085ffe17142099
MarioRodeghiero <mario.rodeghiero@gmail.com>
2017-09-20 00:09:32 -0300

    hooks mocha

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -3,28 +3,33 @@
 // rodar somente um caso: context.only
 // pular o teste: it.skip
 
-describe('Main', function () {
-  describe('Method A', function () {
-    context('case 1', function () {
-      it.skip('should happen test case 1', function () {
+describe('Main 2', function () {
 
-        throw new Error('Erro case 1')
-      });
-    });
-
-    context('case 2', function () {
-      it('should happen test case 2.0', function () {
-
-        throw new Error('Erro case 2')
-      });
-
-      it('should happen test case2.1', function () {
-
-      });
-    });
+// roda uma vez, antes do bloco
+  before(function() {
+    console.log('before');
   });
 
-  describe('Method B', function () {
+// roda uma vez, depois do bloco
+  after(function() {
+    console.log('after');
+  });
 
+// roda todas as vezes, antes de cada bloco
+  beforeEach(function() {
+    console.log('beforeEach');
+  });
+
+// roda todas as vezes, depois de cada bloco
+  afterEach(function() {
+    console.log('afterEach');
+  });
+
+  it('test 1', function() {
+    console.log('test 1');
+  });
+
+  it('test 2', function() {
+    console.log('test 2');
   });
 });
